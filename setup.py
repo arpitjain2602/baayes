087c84a05f75a1168f25a2d5d4925cc39832d5a8
arpitjain2602 <arpit2602@gmail.com>
2021-05-15 22:02:38 +0530

    updating setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'baayes',         # How you named your package folder (MyLib)
   packages = ['baayes'],   # Chose the same as "name"
-  version = '0.1.6',      # Start with a small number and increase it with every change you make
+  version = '0.1.7',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Bayesian Optimization',   # Give a short description about your library
   author = 'Arpit Jain',                   # Type in your name
